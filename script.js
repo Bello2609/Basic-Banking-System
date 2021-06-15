c66c720b02aa1abe65b16a3cd9e6ae4b1aa44879
Alliendeveloper <52082507+Bello2609@users.noreply.github.com>
2021-06-15 15:21:29 +0100

    change the function to es6

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 let myAccountBalance = parseInt(document.getElementById("myAccountBalance").innerText);
 
-function sendMoney(){
+const sendMoney = ()=>{
    var enterName = document.getElementById("enterName").value;
    var enterAmount = parseInt(document.getElementById("enterAmount").value);
 
    if (enterAmount > 8000) {
       alert("Insufficient Balance.")
    } else {
-      var findUserBankAccount = enterName + "BankBalance";
-      var finalAmount = parseInt(document.getElementById(findUserBankAccount).innerHTML) + enterAmount;
+      let findUserBankAccount = enterName + "BankBalance";
+      let finalAmount = parseInt(document.getElementById(findUserBankAccount).innerHTML) + enterAmount;
       var myAccountBalance = parseInt(document.getElementById("myAccountBalance").innerText) - enterAmount
       document.getElementById("myAccountBalance").innerText = myAccountBalance
       document.getElementById(findUserBankAccount).innerHTML = finalAmount;
@@ -16,10 +16,10 @@ function sendMoney(){
       $${enterAmount} is sent to ${enterName}@email.com.`)
 
       // transaction history 
-      var createPTag = document.createElement("li");
-      var textNode = document.createTextNode(`$${enterAmount} is sent to recepient with Email-id ${enterName}@email.com on ${Date()}.`);
+      let createPTag = document.createElement("li");
+      let textNode = document.createTextNode(`$${enterAmount} is sent to recepient with Email-id ${enterName}@email.com on ${Date()}.`);
       createPTag.appendChild(textNode);
-      var element = document.getElementById("transaction-history-body");
+      let element = document.getElementById("transaction-history-body");
       element.insertBefore(createPTag, element.firstChild);
    }
 }
